13fd912bdd967316c3ad492ae7d9c8216add6c85
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-09 16:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var recipes = new Object ({})
 var obj = {prop: 1 }
 
 function updateObjectWithKeyAndValue (object, key, value) {
-  return Object.assign({}, obj, {[key]: value})
+  return Object.assign({}, obj, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
 function deleteFromObjectByKey (object, key, value) {
-  delete object.key
-  return obj
+  delete object.key;
+  return obj;
 }
