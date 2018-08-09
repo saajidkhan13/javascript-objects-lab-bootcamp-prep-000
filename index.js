b187e9c08f12c59dd1e2ecb52b9391836c742d04
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-09 16:50:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var obj = {prop: 1 }
 function updateObjectWithKeyAndValue (object, key, value) {
   return Object.assign({}, obj, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object[key] = value
+  return object
+}
